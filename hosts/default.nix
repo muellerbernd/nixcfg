878b1e4d8c394dc14e5b454269a022ef471ef05e
Bernd Müller <github@muellerbernd.de>
2023-09-24 21:35:11 +0200

    add: podman settings

--- hosts/default.nix
+++ hosts/default.nix
@@ -233,11 +233,27 @@
   #   [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
 
   virtualisation = {
+    containers.enable = true;
+    containers.storage.settings = {
+      storage = {
+        driver = "overlay";
+        runroot = "/run/containers/storage";
+        graphroot = "/var/lib/containers/storage";
+        rootless_storage_path = "/tmp/containers-$USER";
+        options.overlay.mountopt = "nodev,metacopy=on";
+      };
+    };
+    # declare containers
+    oci-containers = {
+      # use podman as default container engine
+      backend = "podman";
+    };
     podman = {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      enableNvidia = true;
 
       defaultNetwork.settings = { dns_enabled = true; };
     };
@@ -249,6 +265,12 @@
     #   };
     # };
   };
+  environment.extraInit = ''
+    if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
+      export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+    fi
+  '';
+
   # programs
   # thunar settings
   programs.thunar = {
