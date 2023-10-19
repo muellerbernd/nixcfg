281b4b249a8646a20695f00adbaa0e580e532208
Bernd Müller <github@muellerbernd.de>
2023-10-19 22:22:27 +0200

    t480 use zramSwap

--- hosts/t480/hardware-configuration.nix
+++ hosts/t480/hardware-configuration.nix
@@ -31,7 +31,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
+  # swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
 
   powerManagement = {
     enable = true;
