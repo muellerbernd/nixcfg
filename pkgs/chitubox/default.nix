ec12c8986e5ce2bcf6f19769ee87e742eb68e1f2
Bernd Müller <github@muellerbernd.de>
2023-12-01 12:35:02 +0100

    add: chitubox, WIP

--- pkgs/chitubox/default.nix
+++ pkgs/chitubox/default.nix
@@ -1,19 +1,33 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
+{ pkgs, stdenv, fetchurl, autoPatchelfHook, libglvnd, libgcrypt, libdrm, zlib, glib
+, fontconfig, freetype }:
 
 stdenv.mkDerivation rec {
   pname = "chitubox";
 
-  # version = "1.9.5";
-  version = "1.7.0";
+  version = "1.9.5";
+  # version = "1.7.0";
 
-  src = builtins.fetchTarball {
-    url = "https://sac.chitubox.com/software/download.do?softwareId=17839&softwareVersionId=v${version}&fileName=CHITUBOX_V${version}.tar.gz";
-    sha256 = "0di0d3hg7jy2c63isdv50c3qsff9vk2x0305jjdqy8xpy62mh9dq";
+  # src = builtins.fetchTarball {
+  #   url = "https://sac.chitubox.com/software/download.do?softwareId=17839&softwareVersionId=v${version}&fileName=CHITUBOX_V${version}.tar.gz";
+  #   sha256 = "0di0d3hg7jy2c63isdv50c3qsff9vk2x0305jjdqy8xpy62mh9dq";
+  # };
+  src = fetchurl {
+    url =
+      "https://sac.chitubox.com/software/download.do?softwareId=17839&softwareVersionId=v${version}&fileName=CHITUBOX_V${version}.tar.gz";
+    hash = "sha256-mNEMfuzRSKBo5tGITWrwg68caLx8Zjz+CaSnbt35Nis=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
 
-  buildInputs = [ stdenv.cc.cc.lib libglvnd libgcrypt zlib glib fontconfig freetype libdrm ];
+  buildInputs = [
+    stdenv.cc.cc.lib
+    libglvnd
+    libgcrypt
+    zlib
+    glib
+    fontconfig
+    freetype
+    libdrm
+  ];
 
   buildPhase = ''
     mkdir -p bin
@@ -42,7 +56,8 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    description = "A Revolutionary Tool to Change 3D Printing Processes within One Click";
+    description =
+      "A Revolutionary Tool to Change 3D Printing Processes within One Click";
     homepage = "https://www.chitubox.com";
     license = {
       fullName = "ChiTuBox EULA";
