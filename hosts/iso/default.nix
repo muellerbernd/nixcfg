2c2ab2c889f88947b94c348673510c7e908800f5
Bernd Müller <github@muellerbernd.de>
2023-12-27 21:41:04 +0100

    fix: custom ISO

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -3,23 +3,7 @@
   # NixOS uses NTFS-3G for NTFS support.
   boot.supportedFilesystems = [ "ntfs" "cifs" ];
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart =
-          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
-
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -76,7 +60,7 @@
       # require public key authentication for better security
       settings.PasswordAuthentication = false;
       # settings.KbdInteractiveAuthentication = false;
-      settings.PermitRootLogin = "no";
+      settings.PermitRootLogin = lib.mkForce "no";
       openFirewall = true;
     };
     # enable blueman
@@ -171,16 +155,6 @@
       keep-derivations = true
       builders-use-substitutes = true
     '';
-    # sshServe = {
-    #   enable = true;
-    #   protocol = "ssh-ng";
-    #   write = "true";
-    #   keys = [
-    #     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCr7tntTSMnedzhPA9ScXtX5JtRlsqQEqZisSXV/gs9Z7eDOrFiLZFVCZ7M+z9U4TvXCkEw1r06ruLcYHim5wSSLhW7tHpFm7fs1CA9gbftbUkKHx8Po+f6tA9J+f60gQHJeG2KPaYzjeBMIc4e+4E4jj0d+zzGUrKTcNu6fMZUT+dA1TR4+sCH5eTi476avLdbAcgYUWnuUJCXixFjjhdalIClcZGFnNFXz3CZfnPiE5tBitAMZJjc4Nkz14PyTQvDH7OSkqQvlBZ8L56SvZSX9ZxEbClgeVUEVI63QYIVjEgeOB4xFr0dpIlPlwAhaBsakr7hmvHpllvMgerUC61Et6T3PWmNO+uAyv0UBcWQG1lMXLlfnN4NfYMoun69kmM/t0KkhT6w2sHjBpuzaoz/0YZSniTOv/Ov5igK/OOwAcshXV0n9Tf31oPqe1UaI6CtyT1qrWgnvxTkTRlxT80g+Ky99BCCCE5BKvFrlq3UziMrRo3NNJ3q/diBhwcvDbc= bernd@t480i7"
-    #
-    #     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/XkOH/IXFjWKWj3RUXP44MHH9P551VeA37MXhl/FeTcitQEomTQzOld6sMzbfca0I0IID25HsXEVEwohp6cHB1sU/YecTu70Ya29r1uqnCmZsnipiNYIAvf8B7GYZZrsWMRn582Cj0vL0zWr5x4SaTydPEifurzUM8DUQLMuN3i1o8yBaUCnqQbjyTec07EpRl15qysWLRW/fg4+fw+V4u91E8X+fUCH63H4pAGRKuXybMlA9q5IDuvTAdlcXi3CiTVp7WKWo0rwkTgzNvvLG7gSyoZu0VCoXW0yTaGjCPg7k7vUsSpUutiIKo8TG1rtEOBS/efzVWc0j1bbBWl2Tgu6JfjYwGfHt//URPvoy4TMJLjoxQ1t3HoiBGhVvSpDeqSD1N2WeutSmArfdlHa0D3hy5lF/uOlEaUhxxxlOOy4F6EPo25JpAiny+UxMuABiH/YmqfuGfJ+TMbZyO9N7ePJuCH/GafLxNjb64yS9ogTGipanb3lQfi+X2zp7ZUU= bernd@x240"
-    #   ];
-    # };
   };
   # nix.envVars.SSH_AUTH_SOCK = "/run/user/1000/gnupg/S.gpg-agent.ssh";
 
@@ -211,43 +185,6 @@
     # nix-serve
     # nix-serve-ng
   ];
-
-  # environment.pathsToLink =
-  #   [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
-
-  virtualisation = {
-    containers.enable = true;
-    containers.storage.settings = {
-      storage = {
-        driver = "overlay";
-        runroot = "/run/containers/storage";
-        graphroot = "/var/lib/containers/storage";
-        rootless_storage_path = "/tmp/containers-$USER";
-        options.overlay.mountopt = "nodev,metacopy=on";
-      };
-    };
-    # declare containers
-    oci-containers = {
-      # use podman as default container engine
-      backend = "podman";
-    };
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      enableNvidia = true;
-
-      defaultNetwork.settings = { dns_enabled = true; };
-    };
-  };
-  environment.extraInit = ''
-    if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
-      export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-    fi
-  '';
-
-  # programs
 
   # use zsh as default shell
   users.defaultUserShell = pkgs.zsh;
@@ -280,19 +217,5 @@
   programs.adb.enable = true;
   programs.wireshark.enable = true;
 
-  zramSwap = {
-    enable = lib.mkDefault true;
-    algorithm = "zstd";
-    memoryPercent = 90;
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  # system.stateVersion = "23.05"; # Did you read the comment?
-  system.stateVersion = "23.11"; # Did you read the comment?
-
+  system.stateVersion = "23.11";
 }
