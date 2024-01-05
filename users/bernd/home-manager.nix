b782c3c9352b21870fed5d5940f0815ed59ec206
Bernd Müller <github@muellerbernd.de>
2024-01-05 13:45:52 +0100

    update homemanager config

--- users/bernd/home-manager.nix
+++ users/bernd/home-manager.nix
@@ -150,12 +150,11 @@
       # rnix-lsp
       nil
       sumneko-lua-language-server
-      nodePackages_latest.bash-language-server
       marksman
       cmake-language-server
       texlab
-      # nodePackages.vscode-langservers-extracted
-      # nodePackages.bash-language-server
+      nodePackages_latest.vscode-langservers-extracted
+      nodePackages_latest.bash-language-server
       gopls
       # rust-analyzer
       # haskell
@@ -212,78 +211,6 @@
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
 
-  # programs = {
-  #   starship = {
-  #     enable = true;
-  #     enableBashIntegration = true;
-  #     settings = {
-  #       username = {
-  #         format = "user: [$user]($style) ";
-  #         show_always = true;
-  #       };
-  #       shlvl = {
-  #         disabled = false;
-  #         format = "$shlvl ▼ ";
-  #         threshold = 4;
-  #       };
-  #     };
-  #   };
-  #   bash = {
-  #     enable = true;
-  #     bashrcExtra = ''
-  #       flash-to(){
-  #         if [ $(${pkgs.file}/bin/file $1 --mime-type -b) == "application/zstd" ]; then
-  #           echo "Flashing zst using zstdcat | dd"
-  #           ( set -x; ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
-  #         elif [ $(${pkgs.file}/bin/file $2 --mime-type -b) == "application/xz" ]; then
-  #           echo "Flashing xz using xzcat | dd"
-  #           ( set -x; ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
-  #         else
-  #           echo "Flashing arbitrary file $1 to $2"
-  #           sudo dd if=$1 of=$2 status=progress conv=sync,noerror bs=64k
-  #         fi
-  #       }
-  #
-  #       export EDITOR=vim
-  #
-  #       mach-shell() {
-  #         pypiApps=$(for arg; do printf '.%s' "$arg"; done)
-  #         nix shell github:davhau/mach-nix#gen.pythonWith$pypiApps
-  #       }
-  #
-  #       # Prints a list of webm urls for a given 4chan thread link
-  #       getwebm() {
-  #         ${pkgs.curl}/bin/curl -sL "$1.json" | ${pkgs.jq}/bin/jq -r '.posts[] | select(.ext == ".webm") | "https://i.4cdn.org/'"$(echo "$1" | sed -r 's/.*(4chan|4channel).org\/([a-zA-Z0-9]+)\/.*/\2/')"'/\(.tim)\(.ext)"';
-  #       }
-  #
-  #       # Makes `nix inate` as an alias of `nix shell`.
-  #       nix() {
-  #         case $1 in
-  #           inate)
-  #             shift
-  #             command nix shell "$@"
-  #             ;;
-  #           *)
-  #             command nix "$@";;
-  #         esac
-  #       }
-  #       encryptFile() {
-  #         cat $1 | ${lib.getExe pkgs.openssl} enc -aes256 -pbkdf2 -base64
-  #       }
-  #       decryptFile() {
-  #         cat $1 | ${lib.getExe pkgs.openssl} aes-256-cbc -d -pbkdf2 -a
-  #       }
-  #     '';
-  #     shellAliases = {
-  #       gr = "cd $(git rev-parse --show-toplevel)";
-  #       n = "nix-shell -p";
-  #       r = "nix repl ${inputs.utils.lib.repl}";
-  #       ssh = "env TERM=xterm-256color ssh";
-  #       ipv6off = "sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 -w net.ipv6.conf.default.disable_ipv6=1 -w net.ipv6.conf.lo.disable_ipv6=1";
-  #       ipv6on = "sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0 -w net.ipv6.conf.default.disable_ipv6=0 -w net.ipv6.conf.lo.disable_ipv6=0";
-  #     };
-  #   };
-  # };
   programs.firefox = {
     enable = true;
 
@@ -334,99 +261,6 @@
 
   };
 
-  #       programs.firefox = {
-  #         enable = true;
-  #         package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-  #             extraPolicies = {
-  #                 CaptivePortal = false;
-  #                 DisableFirefoxStudies = true;
-  #                 DisablePocket = true;
-  #                 DisableTelemetry = true;
-  #                 DisableFirefoxAccounts = false;
-  #                 NoDefaultBookmarks = true;
-  #                 OfferToSaveLogins = false;
-  #                 OfferToSaveLoginsDefault = false;
-  #                 PasswordManagerEnabled = false;
-  #                 FirefoxHome = {
-  #                     Search = true;
-  #                     Pocket = false;
-  #                     Snippets = false;
-  #                     TopSites = false;
-  #                     Highlights = false;
-  #                 };
-  #                 UserMessaging = {
-  #                     ExtensionRecommendations = false;
-  #                     SkipOnboarding = true;
-  #                 };
-  #             };
-  #         };
-  #         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-  #             ublock-origin
-  #             privacy-badger
-  #             https-everywhere
-  #             bitwarden
-  #             clearurls
-  #             decentraleyes
-  #             duckduckgo-privacy-essentials
-  #             floccus
-  #             ghostery
-  #             privacy-redirect
-  #             privacy-badger
-  #             languagetool
-  #             disconnect
-  #             react-devtools
-  #         ];
-  #         profiles = {
-  #             mahmoud = {
-  #                 id = 0;
-  #                 name = "mahmoud";
-  #                 search = {
-  #                     force = true;
-  #                     default = "DuckDuckGo";
-  #                     engines = {
-  #                         "Nix Packages" = {
-  #                             urls = [{
-  #                                 template = "https://search.nixos.org/packages";
-  #                                 params = [
-  #                                     { name = "type"; value = "packages"; }
-  #                                     { name = "query"; value = "{searchTerms}"; }
-  #                                 ];
-  #                             }];
-  #                             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-  #                             definedAliases = [ "@np" ];
-  #                         };
-  #                         "NixOS Wiki" = {
-  #                             urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-  #                             iconUpdateURL = "https://nixos.wiki/favicon.png";
-  #                             updateInterval = 24 * 60 * 60 * 1000;
-  #                             definedAliases = [ "@nw" ];
-  #                         };
-  #                         "Wikipedia (en)".metaData.alias = "@wiki";
-  #                         "Google".metaData.hidden = true;
-  #                         "Amazon.com".metaData.hidden = true;
-  #                         "Bing".metaData.hidden = true;
-  #                         "eBay".metaData.hidden = true;
-  #                     };
-  #                 };
-  #                 settings = {
-  #                     "general.smoothScroll" = true;
-  #                 };
-  #                 extraConfig = ''
-  #                     user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-  #                     user_pref("full-screen-api.ignore-widgets", true);
-  #                     user_pref("media.ffmpeg.vaapi.enabled", true);
-  #                     user_pref("media.rdd-vpx.enabled", true);
-  #                 '';
-  #                 userChrome = ''
-  #                  # a css
-  #                 ";
-  #                 userContent = ''
-  #                  # Here too
-  #                 '';
-  #             };
-  #         };
-  #     };
-  # }
 
   # enable picom
   # services.picom.enable = true;
