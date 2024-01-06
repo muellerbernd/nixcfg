dcd5d03b5266f39093cdb94c27e4eef5915bda91
Bernd Müller <github@muellerbernd.de>
2024-01-07 00:58:03 +0100

    add: sway and greetd

--- modules/mixins/greetd.nix
+++ modules/mixins/greetd.nix
@@ -9,27 +9,30 @@
         command = ''
           ${pkgs.greetd.tuigreet}/bin/tuigreet \
             --time \
+            --remember \
             --asterisks \
             --user-menu \
             --cmd startx
         '';
+        # command = "${pkgs.greetd.greetd}/bin/agreety --cmd startx";
         user = "bernd";
       };
-      sway = {
-        command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
-            --time \
-            --asterisks \
-            --user-menu \
-            --cmd sway
-        '';
-        user = "bernd";
-      };
+      # sway = {
+      #   # command = ''
+      #   #   ${pkgs.greetd.tuigreet}/bin/tuigreet \
+      #   #     --time \
+      #   #     --asterisks \
+      #   #     --user-menu \
+      #   #     --cmd sway
+      #   # '';
+      #   command = "${pkgs.sway}/bin/sway";
+      #   user = "bernd";
+      # };
     };
   };
 
-  environment.etc."greetd/environments".text = ''
-    startx
-    sway
-  '';
+  # environment.etc."greetd/environments".text = ''
+  #   startx
+  #   # sway
+  # '';
 }
