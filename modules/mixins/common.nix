39993401a5147a9a90a34bd36f310f5d973e6cad
Bernd Müller <github@muellerbernd.de>
2024-01-08 07:26:46 +0100

    pipewire settings

--- modules/mixins/common.nix
+++ modules/mixins/common.nix
@@ -124,17 +124,29 @@
   #
   services.xserver.displayManager.startx.enable = true;
 
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+  };
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
   };
 
   # Enable sound.
   sound.enable = true;
   hardware = {
     opengl.enable = true;
-    pulseaudio.enable = false;
     bluetooth = {
       enable = true; # enables support for Bluetooth
       powerOnBoot = true; # powers up the default Bluetooth controller on boot
