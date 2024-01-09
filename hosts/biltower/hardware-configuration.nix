ed3b3f4eb5fe82e061549b6833a9427e15993126
Bernd Müller <github@muellerbernd.de>
2024-01-09 23:31:28 +0100

    biltower

--- hosts/biltower/hardware-configuration.nix
+++ hosts/biltower/hardware-configuration.nix
@@ -22,8 +22,9 @@
     # SSD
     "isci"
   ];
-  boot.initrd.kernelModules =
-    [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+  # boot.initrd.kernelModules =
+  #   [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
