9de98d73014acabf1b9c79f43cd2e4a9792dc238
Bernd Müller <github@muellerbernd.de>
2024-01-12 21:18:22 +0100

    t

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   systems = {
     t480ilmpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAIjDqWjAYK7UcAaaJSzuPyYAXzFonpUL1N2rInH0sgY";
+    t480ammera = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvr23QzFcxZeZeaa3UhnjojhfzKdayQad3YOecygzMs";
     mue-p14s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF75o0sys99U1zvVFWVMsEssGERvqhQ4xLelHTaIyZ9F";
   };
   users = {
@@ -13,5 +14,5 @@ in
 {
   # "secret1.age".publicKeys = [ user1 system1 ];
   # "secret2.age".publicKeys = users ++ systems;
-  "distributedBuilderKey.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s ];
+  "distributedBuilderKey.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s systems.t480ammera ];
 }
