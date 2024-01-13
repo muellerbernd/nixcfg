62c56771096fa6bd3cca96125d0da58be5a604d9
Bernd Müller <github@muellerbernd.de>
2024-01-13 16:10:05 +0100

    homemanager cursor settings

--- users/bernd/home-manager.nix
+++ users/bernd/home-manager.nix
@@ -330,6 +330,32 @@ in
       General.theme = "Adwaita-dark";
     };
   };
+  # not so fancy pointer
+  home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
+  # fancy pointer
+  # home.pointerCursor =
+  #   let
+  #     getFrom = url: hash: name: {
+  #       gtk.enable = true;
+  #       x11.enable = true;
+  #       name = name;
+  #       size = 48;
+  #       package =
+  #         pkgs.runCommand "moveUp" { } ''
+  #           mkdir -p $out/share/icons
+  #           ln -s ${pkgs.fetchzip {
+  #             url = url;
+  #             hash = hash;
+  #           }} $out/share/icons/${name}
+  #         '';
+  #     };
+  #   in
+  #   getFrom
+  #     "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
+  #     "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
+  #     "Fuchsia-Pop";
+
+
 
   # wayland.windowManager.hyprland = {
   #   # Whether to enable Hyprland wayland compositor
