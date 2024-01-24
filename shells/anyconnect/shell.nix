3741cb27d5b0164592fbdab2eb6153e4e7fa896c
Bernd Müller <github@muellerbernd.de>
2024-01-24 21:34:18 +0100

    anyconnect still WIP

--- shells/anyconnect/shell.nix
+++ shells/anyconnect/shell.nix
@@ -6,64 +6,54 @@ let
   anyconnect = pkgs.stdenv.mkDerivation {
     inherit pname version;
 
-    src = ./anyconnect-linux64-4.10.06079-predeploy-k9.tar.gz;
+    # src = ./anyconnect-linux64-4.10.06079-predeploy-k9.tar.gz;
+    srcs = [
+      ./anyconnect-linux64-4.10.06079-predeploy-k9.tar.gz
+      # ./AnyConnectLocalPolicy.xml
+    ];
 
     installPhase =
       # bash
       ''
-        ls
         cd vpn
-        mkdir -p $out/bin
          # install binaries
         for binary in "vpnagentd" "vpn" "vpndownloader" "vpndownloader-cli" "manifesttool_vpn" "acinstallhelper" "vpnui" "acwebhelper" "load_tun.sh"; do
-            install -Dm755 "$binary" "$out/bin/"
+            install -Dm755 $binary "$out/opt/cisco/anyconnect/bin/$binary"
         done
-        mkdir -p $out/lib
-         # install libs
+
+        # install libs
         for lib in "libvpnagentutilities.so" "libvpncommon.so" "libvpncommoncrypt.so" \
             "libvpnapi.so" "libacruntime.so" "libacciscossl.so" "libacciscocrypto.so" \
             "cfom.so" "libboost_date_time.so" "libboost_filesystem.so" "libboost_regex.so" "libboost_system.so" \
             "libboost_thread.so" "libboost_signals.so" "libboost_chrono.so" \
             "libaccurl.so.4.8.0"; do
-            install -Dm755 $lib "$out/lib"
+            install -Dm755 $lib "$out/opt/cisco/anyconnect/lib/lib"
         done
+
         # the installer copies all the other symlinks, but creates this one
         # for some reason so let's just create it ourselves
-        ln -s $out/lib/libaccurl.so.4.8.0 "$out/lib/libaccurl.so.4"
-        mkdir -p $out/bin/plugins
+        ln -s $out/opt/cisco/anyconnect/lib/libaccurl.so.4.8.0 "$out/opt/cisco/anyconnect/lib/libaccurl.so.4"
+
         # install plugins
         # we intentionally don't install the telemetry plugin here
         # because it tries to write to /opt and we don't want that
         for plugin in "libacwebhelper.so" "libvpnipsec.so"; do
-            install -Dm755 $plugin "$out/bin/plugins/$plugin"
+            install -Dm755 $plugin "$out/opt/cisco/anyconnect/bin/plugins/$plugin"
         done
 
-        cp -R resources "$out/resources"
+        cp -R resources "$out/opt/cisco/anyconnect/resources"
 
-        mkdir -p $out/bin/profile
         # install some misc stuff
-        install -Dm444 AnyConnectProfile.xsd "$out/profile/AnyConnectProfile.xsd"
+        install -Dm444 AnyConnectProfile.xsd "$out/opt/cisco/anyconnect/profile/AnyConnectProfile.xsd"
 
         for file in "ACManifestVPN.xml" "update.txt" "AnyConnectLocalPolicy.xsd"; do
-            install -Dm444 $file "$out/$file"
+            install -Dm444 $file "$out/opt/cisco/anyconnect/$file"
         done
 
         # install desktop file for vpnui
         install -Dm644 resources/vpnui48.png "$out/usr/share/icons/hicolor/48x48/apps/cisco-anyconnect.png"
         install -Dm644 resources/vpnui128.png "$out/usr/share/icons/hicolor/128x128/apps/cisco-anyconnect.png"
 
-        sed -i "s|^Exec=.*|Exec=cisco-anyconnect|g" com.cisco.anyconnect.gui.desktop
-        install -Dm644 com.cisco.anyconnect.gui.desktop "$out/usr/share/applications/cisco-anyconnect.desktop"
-
-        # install license
-        for license in "license.txt" "OpenSource.html"; do
-            install -Dm644 $license "$out/usr/share/licenses/cisco-anyconnect/$license"
-        done
-
-        # install systemd unit for vpnagentd
-        install -Dm644 "vpnagentd.service" "$out/usr/lib/systemd/system/vpnagentd.service"
-
-        # install -Dm755 cisco-anyconnect.sh $out/usr/bin/cisco-anyconnect
         # install CA certificates
         mkdir -p "$out/opt/.cisco/certificates/ca"
 
@@ -80,7 +70,8 @@ let
         #
         # this may break some really quirky setups that require downloading files from the server,
         # but there's no other way around it that I could find
-        # install -Dm644 "AnyConnectLocalPolicy.xml" "$out/opt/cisco/anyconnect/AnyConnectLocalPolicy.xml"
+        # ls
+        # install -Dm644 AnyConnectLocalPolicy.xml "$out/opt/cisco/anyconnect/AnyConnectLocalPolicy.xml"
       '';
 
   };
@@ -97,6 +88,23 @@ in
     gdk-pixbuf
     cairo
     libxml2
+    atk
+    at-spi2-atk
+    at-spi2-core
+    boost
+    cairo
+    fakeroot
+    gdk-pixbuf
+    glib
+    gtk3
+    libsoup
+    libxml2
+    libgcc
+    pango
+    ps
+    systemd
+    webkitgtk
+    zlib
   ] ++ (with pkgs.xorg; [
     libX11
     libXcursor
@@ -104,7 +112,7 @@ in
   ]));
   multiPkgs = pkgs: (with pkgs; [
   ]);
-  runScript = "bash";
+  runScript = "zsh";
 
 }).env
 # pkgs.mkShell {
