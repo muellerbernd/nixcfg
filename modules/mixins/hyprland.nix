46818a07b4b20b4d500d705925e9954fe6130f18
Bernd Müller <github@muellerbernd.de>
2024-01-29 12:41:53 +0100

    add: wl-mirror and pipectl

--- modules/mixins/hyprland.nix
+++ modules/mixins/hyprland.nix
@@ -31,6 +31,8 @@
     wlr-randr
     wdisplays
     nwg-look
+    wl-mirror
+    pipectl
   ];
   # xdg-desktop-portal works by exposing a series of D-Bus interfaces
   # known as portals under a well-known name
