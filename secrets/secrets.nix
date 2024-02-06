3930342c338be3cc2ec53207a1818aeb57750cc0
Bernd Müller <github@muellerbernd.de>
2024-02-06 17:00:02 +0100

    agenix setup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
     t480ilmpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAIjDqWjAYK7UcAaaJSzuPyYAXzFonpUL1N2rInH0sgY";
     t480ammera = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvr23QzFcxZeZeaa3UhnjojhfzKdayQad3YOecygzMs";
     mue-p14s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF75o0sys99U1zvVFWVMsEssGERvqhQ4xLelHTaIyZ9F";
+    biltower = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAt7MEJjzVNUPs5KIkE55lw6+Ss6n9EEspuUQJsZm3J";
   };
   users = {
     bernd = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgmYk5cp157HAe1ZKSxcW5/dUgiKTpGi7Jwe0EQqqUe";
@@ -14,7 +15,8 @@ in
 {
   # "secret1.age".publicKeys = [ user1 system1 ];
   # "secret2.age".publicKeys = users ++ systems;
-  "distributedBuilderKey.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s systems.t480ammera ];
+  # "distributedBuilderKey.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s systems.t480ammera ];
+  "distributedBuilderKey.age".publicKeys = allUsers ++ allSystems;
   # "workVpnP14sKey.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s systems.t480ammera ];
-  "workVpnConfig.age".publicKeys = allUsers ++ [ systems.t480ilmpad systems.mue-p14s systems.t480ammera ];
+  "workVpnConfig.age".publicKeys = allUsers ++ allSystems;
 }
