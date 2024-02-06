6ed8a228f4effbfbc48990f48fcbfbf509ab6aa5
Bernd Müller <github@muellerbernd.de>
2024-02-06 12:38:57 +0100

    p14s: setup vpn for work
    Signed-off-by: Bernd Müller <github@muellerbernd.de>

--- hosts/mue-p14s/configuration.nix
+++ hosts/mue-p14s/configuration.nix
@@ -350,9 +350,18 @@
 
   networking.wg-quick.interfaces =
     {
-      wgEIS = {
+      "wgEIS" = {
         configFile = config.age.secrets.workVpnConfig.path;
+        autostart = false;
       };
     };
+  # systemd.services."wg-quick-wgEIS" = {
+  #   requires = [ "network-online.target" ];
+  #   after = [ "network.target" "network-online.target" ];
+  #   wantedBy = lib.mkForce [ ];
+  #   environment.DEVICE = "wgEIS";
+  #   path = [ pkgs.wireguard-tools pkgs.iptables pkgs.iproute ];
+  # };
+
 }
 # vim: set ts=2 sw=2:
