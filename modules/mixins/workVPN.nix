90f022d9d9f936f1619727bafb605d4df7d93b9d
Bernd Müller <github@muellerbernd.de>
2024-02-09 19:42:31 +0100

    workVPN config

--- modules/mixins/workVPN.nix
+++ modules/mixins/workVPN.nix
@@ -1,17 +1,24 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, hostname, ... }:
 {
-  networking.wg-quick.interfaces =
-    {
-      "wgEIS" = {
-        configFile = config.age.secrets.workVpnConfig.path;
-        autostart = false;
-      };
-    };
-  # systemd.services."wg-quick-wgEIS" = {
-  #   requires = [ "network-online.target" ];
-  #   after = [ "network.target" "network-online.target" ];
-  #   wantedBy = lib.mkForce [ ];
-  #   environment.DEVICE = "wgEIS";
-  #   path = [ pkgs.wireguard-tools pkgs.iptables pkgs.iproute ];
-  # };
+  networking.wg-quick.interfaces = {
+    "wgEIS" =
+      if hostname == "mue-p14s" then
+        {
+          configFile = config.age.secrets.workVpnP14sConfig.path;
+          autostart = false;
+        }
+      else
+        {
+          configFile = config.age.secrets.workVpnConfig.path;
+          autostart = false;
+        };
+    # systemd.services."wg-quick-wgEIS" = {
+    #   requires = [ "network-online.target" ];
+    #   after = [ "network.target" "network-online.target" ];
+    #   wantedBy = lib.mkForce [ ];
+    #   environment.DEVICE = "wgEIS";
+    #   path = [ pkgs.wireguard-tools pkgs.iptables pkgs.iproute ];
+    # };
+
+  };
 }
