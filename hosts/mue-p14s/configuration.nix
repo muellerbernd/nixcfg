ba675840fb30acbbc4024bcaefbbef4b5b8dc123
Bernd Müller <github@muellerbernd.de>
2024-04-19 22:00:56 +0200

    [CHANGE] muep14s gstreamer

--- hosts/mue-p14s/configuration.nix
+++ hosts/mue-p14s/configuration.nix
@@ -217,7 +217,25 @@
     hidapi
     libusb
     # openconnect-sso
+    gst_all_1.gstreamer
+    # Common plugins like "filesrc" to combine within e.g. gst-launch
+    gst_all_1.gst-plugins-base
+    # Specialized plugins separated by quality
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    # Plugins to reuse ffmpeg to play almost every video format
+    gst_all_1.gst-libav
+    # Support the Video Audio (Hardware) Acceleration API
+    gst_all_1.gst-vaapi
   ];
+
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+    gst-plugins-good
+    gst-plugins-bad
+    gst-plugins-ugly
+    gst-libav
+  ]);
 
   # Remove this once https://github.com/NixOS/nixpkgs/issues/34638 is resolved
   # The TL;DR is: the kernel calls out to the hard-coded path of
