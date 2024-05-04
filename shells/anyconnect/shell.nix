9f42296508a3897c9e6983b1c3394369aa8fa602
Bernd Müller <github@muellerbernd.de>
2024-05-04 22:52:49 +0200

    [WIP] anyconnect

--- shells/anyconnect/shell.nix
+++ shells/anyconnect/shell.nix
@@ -16,6 +16,11 @@ let
       # bash
       ''
         cd vpn
+        echo $out
+        # sed -i "vpn_install.sh" -e "s|^ *LEGACY_INSTPREFIX=.*$|LEGACY_INSTPREFIX=$out|g"
+        # sed -i "vpn_install.sh" -e "s|^ *INSTPREFIX=.*$|INSTPREFIX=$out|g"
+        # ./vpn_install.sh
+
          # install binaries
         for binary in "vpnagentd" "vpn" "vpndownloader" "vpndownloader-cli" "manifesttool_vpn" "acinstallhelper" "vpnui" "acwebhelper" "load_tun.sh"; do
             install -Dm755 $binary "$out/bin/$binary"
@@ -43,33 +48,33 @@ let
 
         cp -R resources "$out/resources"
 
-        # install some misc stuff
+        # # install some misc stuff
         # install -Dm444 AnyConnectProfile.xsd "$out/opt/cisco/anyconnect/profile/AnyConnectProfile.xsd"
         #
         # for file in "ACManifestVPN.xml" "update.txt" "AnyConnectLocalPolicy.xsd"; do
         #     install -Dm444 $file "$out/opt/cisco/anyconnect/$file"
         # done
-
-        # install desktop file for vpnui
+        #
+        # # install desktop file for vpnui
         # install -Dm644 resources/vpnui48.png "$out/usr/share/icons/hicolor/48x48/apps/cisco-anyconnect.png"
         # install -Dm644 resources/vpnui128.png "$out/usr/share/icons/hicolor/128x128/apps/cisco-anyconnect.png"
-
+        #
         # install CA certificates
         # mkdir -p "$out/opt/.cisco/certificates/ca"
-
-        # first, install our own system root
-        # ln -s /etc/ca-certificates/extracted/tls-ca-bundle.pem "$out/opt/.cisco/certificates/ca/system-ca.pem"
-
-        # then, install Cisco's, because it doesn't actually trace to any of the trusted roots we have
-        # (thanks, VeriSign)
-        # install -Dm644 VeriSignClass3PublicPrimaryCertificationAuthority-G5.pem "$out/opt/.cisco/certificates/ca/VeriSignClass3PublicPrimaryCertificationAuthority-G5.pem"
-
-        # install custom policy to disable auto updates
-        # AnyConnect will attempt to update itself as root, and then run all over both itself and our packaging
-        # so prevent it from doing anything like that
         #
-        # this may break some really quirky setups that require downloading files from the server,
-        # but there's no other way around it that I could find
+        # # first, install our own system root
+        # ln -s /etc/ca-certificates/extracted/tls-ca-bundle.pem "$out/opt/.cisco/certificates/ca/system-ca.pem"
+        #
+        # # then, install Cisco's, because it doesn't actually trace to any of the trusted roots we have
+        # # (thanks, VeriSign)
+        # install -Dm644 VeriSignClass3PublicPrimaryCertificationAuthority-G5.pem "$out/opt/.cisco/certificates/ca/VeriSignClass3PublicPrimaryCertificationAuthority-G5.pem"
+        #
+        # # install custom policy to disable auto updates
+        # # AnyConnect will attempt to update itself as root, and then run all over both itself and our packaging
+        # # so prevent it from doing anything like that
+        # #
+        # # this may break some really quirky setups that require downloading files from the server,
+        # # but there's no other way around it that I could find
         # ls
         # install -Dm644 AnyConnectLocalPolicy.xml "$out/opt/cisco/anyconnect/AnyConnectLocalPolicy.xml"
       '';
