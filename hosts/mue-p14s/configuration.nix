4d408c246f0201d2ab3ef74235fcbad464389308
Bernd Müller <github@muellerbernd.de>
2024-06-05 15:25:41 +0200

    [change] mue-p14s: enable nix-ld

--- hosts/mue-p14s/configuration.nix
+++ hosts/mue-p14s/configuration.nix
@@ -364,11 +364,11 @@
     ensureDefaultPrinter = "HP_Color_LaserJet_flow_MFP_M880";
   };
 
-  # programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #   # Add any missing dynamic libraries for unpackaged programs
-  #   # here, NOT in environment.systemPackages
-  # ];
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 
   # services.icecream.daemon = {
   #   enable = true;
