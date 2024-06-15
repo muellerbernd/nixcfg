b342f269755ab3a6a02905a903b3e0bcf7279c84
Bernd Müller <github@muellerbernd.de>
2024-06-15 23:47:58 +0200

    [change] nixetcup more setup

--- users/bernd/home-manager.nix
+++ users/bernd/home-manager.nix
@@ -166,7 +166,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
+  home.stateVersion = "24.05";
 }
 # vim: set ts=2 sw=2:
 
