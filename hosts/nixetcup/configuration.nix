59816486898b8329d84ae3daad8de2a93e645071
Bernd Müller <github@muellerbernd.de>
2024-06-16 20:38:14 +0200

    [change] nixetcup: keep only a working jitsi setup for now

--- hosts/nixetcup/configuration.nix
+++ hosts/nixetcup/configuration.nix
@@ -8,8 +8,8 @@
   imports = with inputs.self.nixosModules; [
     ./hardware-configuration.nix
     ./nginx.nix
-    # ./jitsi.nix
-    ./xmpp.nix
+    ./jitsi.nix
+    # ./xmpp.nix
     # ./prosody.nix
     # modules
     mixins-server
