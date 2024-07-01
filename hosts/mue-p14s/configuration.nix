1c23ab9eea8c747572e175f8d5435dcaf63e709b
Bernd Müller <github@muellerbernd.de>
2024-07-01 08:11:26 +0200

    [change] mue-p14s: disable systemd encryption

--- hosts/mue-p14s/configuration.nix
+++ hosts/mue-p14s/configuration.nix
@@ -32,10 +32,10 @@
         devices.crypt = {
           device = "/dev/nvme0n1p2";
           preLVM = true;
-          crypttabExtraOpts = ["fido2-device=auto" "token-timeout=5"];
+          # crypttabExtraOpts = ["fido2-device=auto" "token-timeout=5"];
         };
       };
-      systemd.enable = true;
+      # systemd.enable = true;
     };
   };
 
