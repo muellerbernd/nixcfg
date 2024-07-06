894772076480d2f3a9ed64fbaf4d878922bc6731
Bernd Müller <github@muellerbernd.de>
2024-07-06 17:05:23 +0200

    [add] overlay default.nix

---
+++ overlays/default.nix
@@ -0,0 +1,56 @@
+inputs: {
+  # Overlays for personal pkgs (callPackage)
+  additions = final: _: import ../packages { pkgs = final; };
+
+  # Overlays for various pkgs (e.g. broken / too old)
+  # modifications = final: prev: {
+  #   stable = import inputs.nixpkgs-stable {
+  #     inherit (final) system;
+  #     config.allowUnfree = true; # Forgive me Stallman
+  #   };
+  #
+  #   # waybar = prev.waybar.overrideAttrs (oldAttrs: {
+  #   #   mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+  #   # });
+  #
+  #   rofi-emoji-wayland = prev.rofi-emoji.overrideAttrs (_oldAttrs: {
+  #     buildInputs = with final; [
+  #       rofi-wayland-unwrapped
+  #       cairo
+  #       glib
+  #       libnotify
+  #       wl-clipboard
+  #       xclip
+  #       xsel
+  #     ];
+  #   });
+  #
+  #   tdlib = prev.tdlib.overrideAttrs (_oldAttrs: {
+  #     version = "1.8.29";
+  #     src = final.fetchFromGitHub {
+  #       owner = "tdlib";
+  #       repo = "td";
+  #
+  #       # The tdlib authors do not set tags for minor versions, but
+  #       # external programs depending on tdlib constrain the minor
+  #       # version, hence we set a specific commit with a known version.
+  #       rev = "e4796b9bb67dee92d821f1c15e0f263a0941be13";
+  #       sha256 = "07g0wa97w6gw5b04zb27jysradsi9gpksqlw5vrl9g5snl3ys8si";
+  #     };
+  #   });
+  #
+  #   mpv-visualizer = prev.mpvScripts.visualizer.overrideAttrs (_oldAttrs: {
+  #     patches = [ ./patches/visualizer.patch ];
+  #   });
+  # };
+
+  # # TODO: Analize
+  # # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # # be accessible through 'pkgs.unstable'
+  # unstable-packages = final: _prev: {
+  #   unstable = import inputs.nixpkgs-unstable {
+  #     inherit (final) system;
+  #     config.allowUnfree = true;
+  #   };
+  # };
+}
