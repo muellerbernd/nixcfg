abadacae5b11ad5f873393c11175d1a82e500692
Bernd Müller <github@muellerbernd.de>
2024-07-25 10:33:16 +0200

    [change] pipewire: extraConfig

--- modules/mixins/pipewire.nix
+++ modules/mixins/pipewire.nix
@@ -17,16 +17,59 @@
     pulse.enable = true;
     wireplumber.enable = true;
   };
-  # environment.etc = {
-  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-  #     bluez_monitor.properties = {
-  #       ["bluez5.enable-sbc-xq"] = true,
-  #       ["bluez5.enable-msbc"] = true,
-  #       ["bluez5.enable-hw-volume"] = true,
-  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-  #     }
-  #   '';
-  # };
+  services.pipewire.wireplumber.extraConfig = {
+    "wh-1000xm3-ldac-hq" = {
+      "monitor.bluez.rules" = [
+        {
+          matches = [
+            {
+              # Match any bluetooth device with ids equal to that of a WH-1000XM3
+              "device.name" = "~bluez_card.*";
+              "device.product.id" = "0x0cd3";
+              "device.vendor.id" = "usb:054c";
+            }
+          ];
+          actions = {
+            update-props = {
+              # Set quality to high quality instead of the default of auto
+              "bluez5.a2dp.ldac.quality" = "hq";
+            };
+          };
+        }
+      ];
+    };
+    "alsa-disable" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [
+            {
+              "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI1__sink";
+            }
+            {
+              "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI2__sink";
+            }
+            {
+              "node.name" = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI3__sink";
+            }
+          ];
+          actions = {
+            update-props = {
+              "device.disabled" = true;
+              "node.disabled" = true;
+            };
+          };
+        }
+      ];
+    };
+    bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+      };
+    };
+  };
   # hardware.pulseaudio.enable = true;
   # hardware.pulseaudio.support32Bit = true; ## If compatibility with 32-bit applications is desired.
   #
