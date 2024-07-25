d731aeb38bca3dd8e66bbef6f7ad3d3dc173d1f1
Bernd Müller <github@muellerbernd.de>
2024-07-25 16:50:06 +0200

    [change] pipewire: extraConfig

--- modules/mixins/pipewire.nix
+++ modules/mixins/pipewire.nix
@@ -18,26 +18,26 @@
     wireplumber.enable = true;
   };
   services.pipewire.wireplumber.extraConfig = {
-    "wh-1000xm3-ldac-hq" = {
-      "monitor.bluez.rules" = [
-        {
-          matches = [
-            {
-              # Match any bluetooth device with ids equal to that of a WH-1000XM3
-              "device.name" = "~bluez_card.*";
-              "device.product.id" = "0x0cd3";
-              "device.vendor.id" = "usb:054c";
-            }
-          ];
-          actions = {
-            update-props = {
-              # Set quality to high quality instead of the default of auto
-              "bluez5.a2dp.ldac.quality" = "hq";
-            };
-          };
-        }
-      ];
-    };
+    # "wh-1000xm3-ldac-hq" = {
+    #   "monitor.bluez.rules" = [
+    #     {
+    #       matches = [
+    #         {
+    #           # Match any bluetooth device with ids equal to that of a WH-1000XM3
+    #           "device.name" = "~bluez_card.*";
+    #           "device.product.id" = "0x0cd3";
+    #           "device.vendor.id" = "usb:054c";
+    #         }
+    #       ];
+    #       actions = {
+    #         update-props = {
+    #           # Set quality to high quality instead of the default of auto
+    #           "bluez5.a2dp.ldac.quality" = "hq";
+    #         };
+    #       };
+    #     }
+    #   ];
+    # };
     "alsa-disable" = {
       "monitor.alsa.rules" = [
         {
@@ -61,14 +61,14 @@
         }
       ];
     };
-    bluetoothEnhancements = {
-      "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-      };
-    };
+    # bluetoothEnhancements = {
+    #   "monitor.bluez.properties" = {
+    #     "bluez5.enable-sbc-xq" = true;
+    #     "bluez5.enable-msbc" = true;
+    #     "bluez5.enable-hw-volume" = true;
+    #     "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+    #   };
+    # };
   };
   # hardware.pulseaudio.enable = true;
   # hardware.pulseaudio.support32Bit = true; ## If compatibility with 32-bit applications is desired.
