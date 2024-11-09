21f8464b209615abcbcb7e5a636b393a0ce3d050
Bernd Müller <github@muellerbernd.de>
2024-11-09 17:33:06 +0100

    [refactor] balodil

--- hosts/balodil/configuration.nix
+++ hosts/balodil/configuration.nix
@@ -17,7 +17,7 @@
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
-      systemd-boot.configurationLimit = 8;
+      systemd-boot.configurationLimit = 2;
     };
   };
 
@@ -32,56 +32,10 @@
         mesa.drivers
       ];
     };
-    trackpoint = {
-      enable = true;
-      sensitivity = 255;
-    };
   };
 
   networking = {
     networkmanager.enable = true;
-    firewall = {
-      enable = lib.mkForce false;
-      allowedTCPPorts = [
-        80
-        443
-        8080
-        5000
-        445 # samba
-        137
-        138
-        139
-        8000
-        5901
-      ];
-      allowedUDPPorts = [
-        80
-        443
-        8080
-        5000
-        445 # samba
-        137
-        138
-        139
-        4500
-        67
-      ];
-      allowedUDPPortRanges = [
-        {
-          from = 4000;
-          to = 50000;
-        }
-        # # ROS2 needs 7400 + (250 * Domain) + 1
-        # # here Domain is 41 or 42
-        # {
-        #   from = 17650;
-        #   to = 17910;
-        # }
-      ];
-      extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns\n
-        iptables -I INPUT -p udp --dport 67 -j ACCEPT";
-      allowPing = true;
-    };
   };
 
   # environment.systemPackages = with pkgs; [
@@ -93,23 +47,6 @@
     xkb.variant = "";
     #xkbOptions = "ctrl:nocaps";
   };
-
-  services.jitsi-meet = {
-    enable = true;
-    hostName = "127.0.0.1";
-    config = {
-      enableWelcomePage = false;
-      prejoinPageEnabled = true;
-      defaultLang = "fi";
-    };
-    interfaceConfig = {
-      SHOW_JITSI_WATERMARK = false;
-      SHOW_WATERMARK_FOR_GUESTS = false;
-    };
-  };
-  services.jitsi-videobridge.openFirewall = true;
-  security.acme.defaults.email = "me@example.com";
-  security.acme.acceptTerms = true;
 }
 # vim: set ts=2 sw=2:
 
