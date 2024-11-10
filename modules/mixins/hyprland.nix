231f00811cc04fdfe8ad38ce88c473be9d12f3c2
Bernd Müller <github@muellerbernd.de>
2024-11-10 09:55:32 +0100

    [change] hyprland: dont git version

--- modules/mixins/hyprland.nix
+++ modules/mixins/hyprland.nix
@@ -18,9 +18,9 @@
     #   # or inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
     #   debug = true;
     # };
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     # package = pkgs.hyprland-debug;
-    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+    # portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
     # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
     # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
