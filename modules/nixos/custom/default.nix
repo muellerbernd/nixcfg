da1ef9e0d6740071b30d48ae0980551475fecc31
Bernd Müller <github@muellerbernd.de>
2025-01-16 07:59:45 +0100

    [change] gui module

--- modules/nixos/custom/default.nix
+++ modules/nixos/custom/default.nix
@@ -142,23 +142,6 @@ in {
     hardware = {
       enableAllFirmware = true;
       # graphics.enable = true;
-      bluetooth = {
-        enable = true; # enables support for Bluetooth
-        powerOnBoot = true; # powers up the default Bluetooth controller on boot
-        settings = {
-          # General = {
-          #   # Restricts all controllers to the specified transport. Default value
-          #   # is "dual", i.e. both BR/EDR and LE enabled (when supported by the HW).
-          #   # Possible values: "dual", "bredr", "le"
-          #   ControllerMode = "dual";
-          #   Enable = "Source,Sink,Media,Socket";
-          #   Experimental = true;
-          # };
-        };
-      };
-      keyboard.qmk.enable = true;
-      # enable usb-modeswitch (e.g. usb umts sticks)
-      usb-modeswitch.enable = true;
     };
 
     services = {
@@ -174,59 +157,8 @@ in {
         settings.PermitRootLogin = lib.mkDefault "yes";
         openFirewall = lib.mkDefault true;
       };
-      # enable blueman
-      blueman.enable = true;
-      # Enable CUPS to print documents.
-      printing.enable = true;
-
-      avahi = {
-        enable = true;
-        nssmdns4 = true;
-        # for a WiFi printer
-        openFirewall = true;
-      };
-
-      # # Gamecube Controller Adapter
-      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
-      # # Xiaomi Mi 9 Lite
-      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="05c6", ATTRS{idProduct}=="9039", MODE="0666"
-      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="2717", ATTRS{idProduct}=="ff40", MODE="0666"
     };
-    # enable the thunderbolt daemon
-    services.hardware.bolt.enable = true;
-    # ignore laptop lid
-    services.logind.lidSwitchDocked = "ignore";
-    services.logind.lidSwitch = "ignore";
-    services.logind.extraConfig = ''
-      # want to be able to listen to music while laptop closed
-      LidSwitchIgnoreInhibited=no
-      HandleLidSwitch=ignore
-      # don’t shutdown when power button is short-pressed
-      HandlePowerKey=ignore
-    '';
 
-    # based on https://cubiclenate.com/2024/02/27/disable-input-devices-in-wayland/
-    systemd.services.toggleLaptopKeyboard = lib.mkDefault {
-      enable = true;
-      wantedBy = ["multi-user.target"];
-      description = ".";
-      serviceConfig = let
-        toggleLaptopKeyboardScript = pkgs.writeShellScriptBin "toggleLaptopKeyboardScript" ''
-          pipe=/tmp/laptopKeyboardState
-          target=/sys/devices/platform/i8042/serio0/input/input0/inhibited
-          [ -p "$pipe" ] || mkfifo -m 0666 "$pipe" || exit 1
-          while :; do
-              while read -r val; do
-                  if [ "$val" ]; then
-                      echo "$val" > $target
-                  fi
-              done <"$pipe"
-          done
-        '';
-      in {
-        ExecStart = ''${toggleLaptopKeyboardScript}/bin/toggleLaptopKeyboardScript'';
-      };
-    };
     # environment.shellInit = ''
     #   [ -n "$DISPLAY" ] && xhost +si:localuser:$USER || true
     # '';
@@ -286,12 +218,12 @@ in {
           8384
           22000
         ];
-        allowedUDPPortRanges = lib.mkDefault [
-          {
-            from = 4000;
-            to = 50000;
-          }
-        ];
+        # allowedUDPPortRanges = lib.mkDefault [
+        #   {
+        #     from = 4000;
+        #     to = 50000;
+        #   }
+        # ];
         # # ROS2 needs 7400 + (250 * Domain) + 1
         # # here Domain is 41 or 42
         # {
--- modules/nixos/custom/gui/default.nix
+++ modules/nixos/custom/gui/default.nix
@@ -23,5 +23,80 @@ in {
     xdg.portal.enable = true;
     services.displayManager.ly.enable = true;
     programs.river.enable = true;
+    hardware = {
+      enableAllFirmware = true;
+      # graphics.enable = true;
+      bluetooth = {
+        enable = true; # enables support for Bluetooth
+        powerOnBoot = true; # powers up the default Bluetooth controller on boot
+        settings = {
+          # General = {
+          #   # Restricts all controllers to the specified transport. Default value
+          #   # is "dual", i.e. both BR/EDR and LE enabled (when supported by the HW).
+          #   # Possible values: "dual", "bredr", "le"
+          #   ControllerMode = "dual";
+          #   Enable = "Source,Sink,Media,Socket";
+          #   Experimental = true;
+          # };
+        };
+      };
+      keyboard.qmk.enable = true;
+      # enable usb-modeswitch (e.g. usb umts sticks)
+      usb-modeswitch.enable = true;
+    };
+
+    services = {
+      # enable blueman
+      blueman.enable = true;
+      # Enable CUPS to print documents.
+      printing.enable = true;
+
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        # for a WiFi printer
+        openFirewall = true;
+      };
+
+      # # Gamecube Controller Adapter
+      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
+      # # Xiaomi Mi 9 Lite
+      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="05c6", ATTRS{idProduct}=="9039", MODE="0666"
+      # SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="2717", ATTRS{idProduct}=="ff40", MODE="0666"
+    };
+    # enable the thunderbolt daemon
+    services.hardware.bolt.enable = true;
+    # ignore laptop lid
+    services.logind.lidSwitchDocked = "ignore";
+    services.logind.lidSwitch = "ignore";
+    services.logind.extraConfig = ''
+      # want to be able to listen to music while laptop closed
+      LidSwitchIgnoreInhibited=no
+      HandleLidSwitch=ignore
+      # don’t shutdown when power button is short-pressed
+      HandlePowerKey=ignore
+    '';
+    # based on https://cubiclenate.com/2024/02/27/disable-input-devices-in-wayland/
+    systemd.services.toggleLaptopKeyboard = lib.mkDefault {
+      enable = true;
+      wantedBy = ["multi-user.target"];
+      description = ".";
+      serviceConfig = let
+        toggleLaptopKeyboardScript = pkgs.writeShellScriptBin "toggleLaptopKeyboardScript" ''
+          pipe=/tmp/laptopKeyboardState
+          target=/sys/devices/platform/i8042/serio0/input/input0/inhibited
+          [ -p "$pipe" ] || mkfifo -m 0666 "$pipe" || exit 1
+          while :; do
+              while read -r val; do
+                  if [ "$val" ]; then
+                      echo "$val" > $target
+                  fi
+              done <"$pipe"
+          done
+        '';
+      in {
+        ExecStart = ''${toggleLaptopKeyboardScript}/bin/toggleLaptopKeyboardScript'';
+      };
+    };
   };
 }
