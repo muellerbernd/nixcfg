caa6b556dbc526b6889b6368553820c2969e47aa
Bernd Müller <github@muellerbernd.de>
2025-02-12 18:46:20 +0100

    [add] zenoh-c package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,6 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   uvtools = pkgs.callPackage ./uvtools {};
   annotator = pkgs.callPackage ./annotator {};
+  zenoh-c = pkgs.callPackage ./zenoh-c {};
   # prusa-slicer = pkgs.callPackage ./prusa-slicer {};
 }
