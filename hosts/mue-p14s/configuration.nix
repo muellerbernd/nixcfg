0ef20ac01cd4ad643e5b79d2addf031f4dcba6e2
Bernd Müller <github@muellerbernd.de>
2025-03-07 14:13:31 +0100

    [fix] throttling p14s

--- hosts/mue-p14s/configuration.nix
+++ hosts/mue-p14s/configuration.nix
@@ -60,11 +60,11 @@
       settings = {
         # CPU_BOOST_ON_AC = 1;
         # CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_AC = "ondemand";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        #
+        # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         # CPU_MIN_PERF_ON_AC = 0;
         # CPU_MAX_PERF_ON_AC = 100;
@@ -74,7 +74,7 @@
         STOP_CHARGE_THRESH_BAT0 = "95";
       };
     };
-    throttled.enable = true;
+    # throttled.enable = true;
     upower.enable = true;
     fwupd.enable = true;
   };
@@ -229,7 +229,7 @@
     throttled
     drawio
     pandoc-drawio-filter
-    nomachine-client
+    # nomachine-client
 
     can-utils
     savvycan
