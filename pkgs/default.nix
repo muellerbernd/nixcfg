1da6f61f91164b32a6fd42990441d8fb22170e1b
Bernd Müller <github@muellerbernd.de>
2025-07-14 11:11:44 +0200

    [add] networkmanager-openconnect

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,4 +6,5 @@
   zenoh-c = pkgs.callPackage ./zenoh-c {};
   zenoh-cpp = pkgs.callPackage ./zenoh-cpp {};
   # prusa-slicer = pkgs.callPackage ./prusa-slicer {};
+  networkmanager-openconnect = pkgs.callPackage ./networkmanager-openconnect/ {};
 }
