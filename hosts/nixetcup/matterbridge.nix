4b1a04f4523a851828ea729fffa774fac2fc2921
Bernd Müller <github@muellerbernd.de>
2025-10-20 11:15:16 +0200

    [refactor] nixetcup: trigger matterbridge restart every day

--- hosts/nixetcup/matterbridge.nix
+++ hosts/nixetcup/matterbridge.nix
@@ -15,4 +15,14 @@
     configPath = config.age.secrets.matterbridgeConfig.path;
     user = "matterbridge";
   };
+  # A daily timer that triggers the restart service at 12:00
+  systemd.timers.matterbridgeRestart = {
+    enable = true;
+    timerConfig = {
+      # run daily at 12:00
+      OnCalendar = "*-*-* 12:00:00 Europe/Berlin";
+      Unit = "matterbridge.service"; # fire the restart service
+      Persistent = true;
+    };
+  };
 }
