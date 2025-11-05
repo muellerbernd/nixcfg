b2e06b5bd385b8798243ed0c82cdeb29927c3a61
Bernd Müller <github@muellerbernd.de>
2025-11-05 07:33:48 +0100

    [add] ammera22: paperless

--- hosts/ammera22-proxmox-vm/configuration.nix
+++ hosts/ammera22-proxmox-vm/configuration.nix
@@ -14,6 +14,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./homeassistant.nix
+    ./paperless.nix
     customSystem
   ];
 
---
+++ hosts/ammera22-proxmox-vm/paperless.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  environment.etc."paperless-admin-pass".text = "admin";
+  services.paperless = {
+    passwordFile = "/etc/paperless-admin-pass";
+    enable = true;
+    consumptionDirIsPublic = true;
+    settings = {
+      PAPERLESS_CONSUMER_IGNORE_PATTERN = [
+        ".DS_STORE/*"
+        "desktop.ini"
+      ];
+      PAPERLESS_OCR_LANGUAGE = "deu+eng";
+      PAPERLESS_OCR_USER_ARGS = {
+        optimize = 1;
+        pdfa_image_compression = "lossless";
+      };
+      # PAPERLESS_URL = "https://paperless.example.com";
+    };
+  };
+
+}
