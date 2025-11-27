3fe6faf968195b7f3f4066ea28a6a054409083f4
Bernd Müller <github@muellerbernd.de>
2025-11-27 08:07:29 +0100

    [change] matterbridge

--- hosts/nixetcup/matterbridge.nix
+++ hosts/nixetcup/matterbridge.nix
@@ -16,23 +16,23 @@
     user = "matterbridge";
   };
 
-  # systemd.services.matterbridgeRestart = {
-  #   enable = true;
-  #   description = "restart matterbridge";
-  #   serviceConfig = {
-  #     ExecStart = "systemctl restart matterbridge.service";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  # };
-  #
-  # # A daily timer that triggers the restart service at 12:00
-  # systemd.timers.matterbridgeRestart = {
-  #   enable = true;
-  #   timerConfig = {
-  #     # run daily at 13:00
-  #     OnCalendar = "*-*-* 12:22:00 Europe/Berlin";
-  #     Unit = "matterbridgeRestart.service"; # fire the restart service
-  #     Persistent = true;
-  #   };
-  # };
+  systemd.services.matterbridgeRestart = {
+    enable = true;
+    description = "restart matterbridge";
+    serviceConfig = {
+      ExecStart = "systemctl restart matterbridge.service";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  # A daily timer that triggers the restart service at 12:00
+  systemd.timers.matterbridgeRestart = {
+    enable = true;
+    timerConfig = {
+      # run daily at 13:00
+      OnCalendar = "*-*-* 12:22:00 Europe/Berlin";
+      Unit = "matterbridgeRestart.service"; # fire the restart service
+      Persistent = true;
+    };
+  };
 }
